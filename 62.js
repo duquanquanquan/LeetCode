a207afcac65c26e4b34bc198f0989f4821c7cdbe
duyuanyuan <1454715282@qq.com>
2019-07-06 22:08:58 +0800

    Unique Paths

---
+++ 62.js
@@ -0,0 +1,20 @@
+
+let uniquePaths = function(m, n) {
+    let dp = [];
+    for (let i = 0; i < m; i++) {
+        dp.push([])
+    }
+    for (let i = 0; i < n; i++) {
+        dp[0][i] = 1
+    }
+    for (let i = 1; i < m; i++) {
+        dp[i][0] = 1;
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        }
+    }
+    return dp[m-1][n-1]
+};
+
+console.log(uniquePaths(3, 7));
+
